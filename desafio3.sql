f18f96e092b2d3e03e2ba41fdb895aec3afee23e
danricardo88 <daniel.ricardo88@outlook.com>
2022-10-28 15:45:55 -0300

    req-3

--- desafio3.sql
+++ desafio3.sql
@@ -2,11 +2,13 @@ SELECT
   U.usuario_nome AS usuario,
   COUNT(*) AS qt_de_musicas_ouvidas,
   FORMAT(SUM(S.duracao) / 60, 2) AS total_minutos
+  
 FROM
   SpotifyClone.tabela_usuario AS U
   INNER JOIN SpotifyClone.tabela_de_reproducoes AS P ON P.usuario_id = U.usuario_id
   INNER JOIN SpotifyClone.tabela_musicas AS S ON P.musica_id = S.musica_id
+  
 GROUP BY
-  U.usuario_name
+  U.usuario_nome
 ORDER BY
-  U.usuario_name;
+  U.usuario_nome;
