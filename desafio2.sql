b766bf48fede648789b641f2bb3bc26f743da110
danricardo88 <daniel.ricardo88@outlook.com>
2022-10-28 15:45:25 -0300

    req-3

--- desafio2.sql
+++ desafio2.sql
@@ -2,3 +2,13 @@ SELECT
 (SELECT COUNT(*) FROM tabela_musicas) AS cancoes,
 (SELECT COUNT(*) FROM tabela_artista) AS artistas,
 (SELECT COUNT(*) FROM tabela_albuns) AS albuns;
+
+
+-- SELECT
+-- COUNT(DISTINCT C.nome_musica) AS cancoes,
+-- COUNT(DISTINCT A.artista_nome) AS artistas,
+-- COUNT(DISTINCT AL.album_nome) AS albuns
+-- FROM
+--   SpotifyClone.tabela_artista AS A
+--   JOIN SpotifyClone.tabela_albuns AS AL
+--   JOIN SpotifyClone.tabela_musicas AS C;
